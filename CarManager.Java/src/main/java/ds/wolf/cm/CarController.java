24783255dc3a8b5e70e49c254b26b3045f8a3417
wolf <intelliwolf@163.com>
2014-11-21 13:34:14 +0800

    add some java file.

--- CarManager.Java/src/main/java/ds/wolf/cm/CarController.java
+++ CarManager.Java/src/main/java/ds/wolf/cm/CarController.java
@@ -1,11 +1,16 @@
 package ds.wolf.cm;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by wolf on 2014/11/20.
@@ -14,12 +19,9 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class CarController {
 
-
+    private  String hello="helloWorld";
     @RequestMapping(value = "/hello")
-    public
-    @ResponseBody
-    String getMessage(@RequestParam String name) {
-        HttpServletRequest request
+    public String getMessage(HttpServletRequest request, HttpServletResponse response){
         String msg;
         if (request.getParameterMap().containsKey("state")) {
             String state = request.getParameter("state");
@@ -28,8 +30,30 @@ public class CarController {
         } else {
             msg = "current state!";
         }
+        try {
+            response.getOutputStream().println(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/hellox")
+    public
+    @ResponseBody
+    String getMessageX(@RequestParam String name) {
+        String msg;
+         msg = name;
         return msg;
-        return "hello wolf!!!!!!!!!!";
+    }
+
+    @RequestMapping(value = "/import",method = RequestMethod.POST)
+    public void importFile(HttpServletRequest request, HttpServletResponse response){
+
+    }
+    @RequestMapping(value = "/import",method = RequestMethod.POST)
+    public void importFilex( @RequestParam("file") MultipartFile file){
+
     }
 
 }
